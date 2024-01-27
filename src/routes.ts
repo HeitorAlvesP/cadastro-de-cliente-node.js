fa9941c06cde30fc74488a54173d7680ea57b628
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-01-27 11:04:29 -0300

    new rutes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreatCustemeControllers } from "./controllers/CreatCustemeControllers";
 
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     
     fastify.get("/teste", async(request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
+    })
+
+    fastify.post("/customer", async (request: FastifyRequest, replay: FastifyReply) => {
+        return new CreatCustemeControllers().handle(request, replay)
     })
 
 }
